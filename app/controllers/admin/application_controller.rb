159ec5dbac21e0f60cc73fc4c29ad33c16bd164e
feycot <feycot@gmail.com>
2021-08-19 00:26:42 +0500

    fix invalid nested admin app controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,16 +1,14 @@
 class Admin::ApplicationController < ApplicationController
-  class Admin::ApplicationController < ApplicationController
-    include AuthHelper
-    helper_method :current_user
+  include AuthHelper
+  helper_method :current_user
 
-    before_action :authenticate_user!, :authorize
+  before_action :authenticate_user!, :authorize
 
-    def authorize
-      render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false) if forbidden?
-    end
+  def authorize
+    render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false) if forbidden?
+  end
 
-    def forbidden?
-      !current_user.is_a?(Admin)
-    end
+  def forbidden?
+    !current_user.is_a?(Admin)
   end
 end
