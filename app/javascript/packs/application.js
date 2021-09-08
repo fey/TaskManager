6b5c3275b6915e128757e69f63296e3dd394e60b
feycot <feycot@gmail.com>
2021-09-08 09:32:46 +0500

    add TasksRepository

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import 'material-design-lite/material.js';
-import WebpackerReact from 'webpacker-react'
-import TaskBoard from '../components/TaskBoard'
+import WebpackerReact from 'webpacker-react';
+import TaskBoard from '../components/TaskBoard';
 
 Rails.start();
 ActiveStorage.start();
-WebpackerReact.setup({ TaskBoard })
+WebpackerReact.setup({ TaskBoard });
